02891c30abd4a59fc4faf5eced6e1a074e6bd75e
unknown <juliamouraruela@gmail.com>
2026-05-05 00:15:09 -0300

    teste

--- RA2/AULA 9/EX5.py
+++ RA2/AULA 9/EX5.py
@@ -1,2 +1,5 @@
 dados = {'a': 10} #declara um valor a chave a
 print(dados.get('b', 5))  #o get tenta acessar a chave 'b' e, como ela não existe, retorna o valor padrão 5 e printa o resultado. Se nãao tivesse 5 printaria None
+
+
+#oi
